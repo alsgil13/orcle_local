97e60f473f2027cdc62356ed1ef4d20d4cc852f8
alsgil13 <als.gil13@gmail.com>
2019-12-10 16:26:13 -0300

    Arrumando static path

--- orcle_django/settings.py
+++ orcle_django/settings.py
@@ -126,7 +126,7 @@ DATE_INPUT_FORMATS = ('%d/%m/%Y','%Y-%m-%d')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '../static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
